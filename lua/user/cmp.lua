1d0c20f910a195977b018b9b552da955552b0a64
Pyxels <39232833+Pyxels@users.noreply.github.com>
2022-07-08 11:30:21 +0200

    feat(snippets): add custom snippets and inlay hint if in snippet

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -13,7 +13,6 @@ if not copilot_status_ok then
 	return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
 
 local check_backspace = function()
 	local col = vim.fn.col(".") - 1
---
+++ lua/user/luasnip.lua
@@ -0,0 +1,56 @@
+local ls = require("luasnip")
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+-- some shorthands...
+local snip = ls.snippet
+local node = ls.snippet_node
+local text = ls.text_node
+local insert = ls.insert_node
+local func = ls.function_node
+local choice = ls.choice_node
+local dynamicn = ls.dynamic_node
+
+ls.config.set_config({
+	history = true,
+	-- treesitter-hl has 100, use something higher (default is 200).
+	ext_base_prio = 200,
+	-- minimal increase in priority.
+	ext_prio_increase = 1,
+	enable_autosnippets = false,
+	store_selection_keys = "<c-s>",
+})
+
+ls.add_snippets(nil, {
+  -- LaTex snippets
+  tex = {
+    snip({
+      trig = "quote",
+      namr = "Quote",
+      dscr = "German style double quote",
+    }, {
+        text "\\glqq ",
+        insert(1, "quote"),
+        text "\\grqq{} ",
+        insert(0),
+      })
+  },
+
+})
+
+-- show inlay hint when having a choice or inserting
+local types = require("luasnip.util.types")
+require'luasnip'.config.setup({
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = {{"", "#fe8019"}}
+			}
+		},
+		[types.insertNode] = {
+			active = {
+				virt_text = {{"", "#83a598"}}
+			}
+		}
+	},
+})
