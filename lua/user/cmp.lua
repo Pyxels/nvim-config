f40d9e4cdfdbfbdad1bc3337d0b1c3c6437d2700
Pyxels <39232833+Pyxels@users.noreply.github.com>
2022-07-23 09:52:41 +0200

    refactor(cmp): remove lengthy configuration into their own local variables

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -49,79 +49,85 @@ local custom_colors = {
   copilot = 'CmpItemKindCopilot',
 }
 
+local custom_mapping = {
+  ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+  ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+  ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+  ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+  ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
+  ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
+  ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+  ['<C-y>'] = cmp.mapping(
+    cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+    { 'i', 'c' }
+  ),
+  ['<C-e>'] = cmp.mapping({
+    i = cmp.mapping.abort(),
+    c = cmp.mapping.close(),
+  }),
+  ['<C-l>'] = cmp.mapping(function()
+    luasnip.jump(1)
+  end, { 'i', 's' }),
+  ['<C-h>'] = cmp.mapping(function()
+    luasnip.jump(-1)
+  end, { 'i', 's' }),
+  ['<A-l>'] = cmp.mapping(function()
+    luasnip.change_choice(1)
+  end, { 'i', 's' }),
+  ['<A-h>'] = cmp.mapping(function()
+    luasnip.change_choice(-1)
+  end, { 'i', 's' }),
+  ['<C-c>'] = cmp.mapping(function()
+    if luasnip.choice_active() then
+      select_choice()
+    end
+  end, { 'i', 's' }),
+}
+
+local custom_formatting = {
+  fields = { 'kind', 'abbr', 'menu' },
+  format = function(entry, vim_item)
+    -- Kind icons
+    vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
+    -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+    vim_item.menu = ({
+      copilot = '[Copilot]',
+      nvim_lsp = '[LSP]',
+      crates = '[Crates]',
+      luasnip = '[Snippet]',
+      buffer = '[Buffer]',
+      path = '[Path]',
+    })[entry.source.name]
+
+    if custom_colors[entry.source.name] then
+      vim_item.kind_hl_group = custom_colors[entry.source.name]
+    end
+
+    return vim_item
+  end,
+}
+
+local custom_sources = {
+  { name = 'copilot' },
+  { name = 'nvim_lsp' },
+  { name = 'crates' },
+  { name = 'luasnip' },
+  { name = 'buffer' },
+  { name = 'path' },
+}
+
 cmp.setup({
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
-  mapping = {
-    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.mapping(
-      cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      }),
-      { 'i', 'c' }
-    ),
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<C-l>'] = cmp.mapping(function()
-      luasnip.jump(1)
-    end, { 'i', 's' }),
-    ['<C-h>'] = cmp.mapping(function()
-      luasnip.jump(-1)
-    end, { 'i', 's' }),
-    ['<A-l>'] = cmp.mapping(function()
-      luasnip.change_choice(1)
-    end, { 'i', 's' }),
-    ['<A-h>'] = cmp.mapping(function()
-      luasnip.change_choice(-1)
-    end, { 'i', 's' }),
-    ['<C-c>'] = cmp.mapping(function()
-      if luasnip.choice_active() then
-        select_choice()
-      end
-    end, { 'i', 's' }),
-  },
-  formatting = {
-    fields = { 'kind', 'abbr', 'menu' },
-    format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-      vim_item.menu = ({
-        copilot = '[Copilot]',
-        nvim_lsp = '[LSP]',
-        crates = '[Crates]',
-        luasnip = '[Snippet]',
-        buffer = '[Buffer]',
-        path = '[Path]',
-      })[entry.source.name]
-
-      if custom_colors[entry.source.name] then
-        vim_item.kind_hl_group = custom_colors[entry.source.name]
-      end
-
-      return vim_item
-    end,
-  },
-  sources = {
-    { name = 'copilot' },
-    { name = 'nvim_lsp' },
-    { name = 'crates' },
-    { name = 'luasnip' },
-    { name = 'buffer' },
-    { name = 'path' },
-  },
+  mapping = custom_mapping,
+  formatting = custom_formatting,
+  sources = custom_sources,
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
