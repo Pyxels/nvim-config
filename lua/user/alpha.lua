689aafa68f2141f02aedf99c7a72ccb775082e75
Pyxels <39232833+Pyxels@users.noreply.github.com>
2022-12-03 16:33:01 +0100

    feat(alpha): configuration shortcut now goes directly to the plugins file

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -25,7 +25,7 @@ dashboard.section.buttons.val = {
   dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
   dashboard.button('w', '龎 Vim Wiki', ':VimwikiIndex <CR>'),
   dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
-  dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.lua <CR>'),
+  dashboard.button('c', '  Configuration', ':e ~/.config/nvim/lua/user/plugins.lua <CR>'),
   dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
 }
 
