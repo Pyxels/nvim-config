ec2069c3de3cf81fa3d8e719c57bc50d615f0363
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-01-09 11:16:25 +0100

    fix(plugins): remove unused plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -42,7 +42,6 @@ packer.init({
 return packer.startup(function(use)
   -- My plugins here
   use('wbthomason/packer.nvim') -- Have packer manage itself
-  use('nvim-lua/popup.nvim') -- An implementation of the Popup API from vim in Neovim
   use('nvim-lua/plenary.nvim') -- Useful lua functions used in lots of plugins
   use('numToStr/Comment.nvim') -- Easily comment stuff
   use('kyazdani42/nvim-web-devicons')
