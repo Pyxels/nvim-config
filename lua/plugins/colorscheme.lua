6ebdb37e569115c420351d907609385cdae876a5
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-01-10 18:57:09 +0100

    refactor(colorscheme): remove unecessary color setting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,10 +10,6 @@ return {
       -- Enable transparent mode
       vim.g.gruvbox_baby_transparent_mode = 0
 
-      -- Set background color in visual mode
-      local colors = require('gruvbox-baby.colors').config()
-      vim.g.gruvbox_baby_highlights = { Visual = { bg = colors.medium_gray } }
-
       -- Load the colorscheme
       vim.cmd([[
 try
