0e49d646746a8f449646289a1cf981a48a84decb
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-01-11 18:24:52 +0100

    feat(alpha): add lazy shortcut and startup time in footer to alpha

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -200,18 +200,43 @@ return {
         dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
         dashboard.button('w', '龎 Vim Wiki', ':VimwikiIndex <CR>'),
         dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
-        dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.lua <CR>'),
+        dashboard.button('c', '  Configuration', ':e $MYVIMRC <CR>'),
+        dashboard.button('l', '鈴' .. ' Lazy', ':Lazy<CR>'),
         dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
       }
-
-      dashboard.section.footer.val = 'Basically an IDE'
 
       dashboard.section.footer.opts.hl = 'Type'
       dashboard.section.header.opts.hl = 'Include'
       dashboard.section.buttons.opts.hl = 'Keyword'
 
       dashboard.opts.opts.noautocmd = true
+      return dashboard
+    end,
+    config = function(_, dashboard)
+      vim.b.miniindentscope_disable = true
+
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == 'lazy' then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'AlphaReady',
+          callback = function()
+            require('lazy').show()
+          end,
+        })
+      end
+
       require('alpha').setup(dashboard.opts)
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'LazyVimStarted',
+        callback = function()
+          local stats = require('lazy').stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          dashboard.section.footer.val = '⚡ Neovim loaded ' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
     end,
     keys = { { '<Leader>a', '<cmd>Alpha<cr>', desc = 'Open [A]lpha' } },
   },
