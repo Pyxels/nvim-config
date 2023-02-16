da6fd8395e617c0750e20f33c1406c2612c559b8
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-02-16 09:58:03 +0100

    feat(git): change status gutter bars to be wider

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -63,11 +63,11 @@ return {
     },
     opts = {
       signs = {
-        add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+        add = { hl = 'GitSignsAdd', text = '▐', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+        change = { hl = 'GitSignsChange', text = '▐', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
         delete = { hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
         topdelete = { hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+        changedelete = { hl = 'GitSignsChange', text = '▐', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
       },
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
